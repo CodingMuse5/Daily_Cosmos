fb7e8b79d97b73ca0a89f7848f9ef20efb295749
Shreya <shreyasawant5.work@gmail.com>
2026-01-02 19:17:57 +0530

    Update API URL to daily-cosmos-1

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 // FORCE the app to use the Render Cloud URL
 // We are hardcoding it so it CANNOT fail to find it.
 const api = axios.create({
-  baseURL: 'https://daily-cosmos.onrender.com/api'
+  // Update this line below with your NEW URL (daily-cosmos-1)
+  baseURL: 'https://daily-cosmos-1.onrender.com/api' 
 });
 
 export default api;
